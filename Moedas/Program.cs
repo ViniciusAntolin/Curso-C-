a07c227c208c5de1a6fc58d3b8d04ccff5719163
Vinicius Pires Antolin <vinicius.pires@inovafarma.com.br>
2023-10-28 22:49:26 -0300

    feat add git ignore

---
+++ Moedas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.90m; //MOEDAS EM GERAL USAR DECIMAL, SE NÃO ATENDER, UTILIZA OUTRO TIPO DE DADOS.
+            Console.WriteLine(valor.ToString("C",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valor.ToString("N",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+        }
+    }
+}
